b50a5bb5c1a86097e5459fb95257c7e9ed3520bc
User <User@DESKTOP-5H96RJI>
2017-05-08 13:27:14 +0200

    proyecto

---
+++ ALMACEN/Login.vb
@@ -0,0 +1,10 @@
+﻿Public Class Login
+    Private Sub btnVolver_Click(sender As Object, e As EventArgs) Handles btnVolver.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnRegistrarse_Click(sender As Object, e As EventArgs) Handles btnRegistrarse.Click
+        Dim registrarse As New Registrarse
+        registrarse.ShowDialog()
+    End Sub
+End Class
--- ALMACEN/administrador.vb
+++ ALMACEN/administrador.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Administrador
 
+
+    Private Sub btnEntrarAdmin_Click(sender As Object, e As EventArgs) Handles btnEntrarAdmin.Click
+        If txtAdmin.Text = "miriam" AndAlso txtContraseñaAdmin.Text = "miriam" Then
+            MsgBox("Bienvenida Miriam")
+        Else
+            MsgBox("Sentimos las molestias pero usted no tiene permiso para entrar a modo administrador. Gracias por su compresión")
+        End If
+    End Sub
 End Class
